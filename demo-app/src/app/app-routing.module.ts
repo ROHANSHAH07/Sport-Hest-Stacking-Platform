44f919eed33d53c3aa117427d2974d9c966109ba
Rohanshah07399cool <rohan07399@yopmail.com>
2022-04-12 11:44:41 +0530

    Add Routing in app routing file.

--- demo-app/src/app/app-routing.module.ts
+++ demo-app/src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo:'signin',
+    pathMatch:  'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'signup',
+    component:SignupComponent
+  },
+  {
+    path:'signin',
+    component:SigninComponent
+  },
+  {
+    path:'forgot-password',
+    component:ForgotPasswordComponent
+  },
+  {
+    path:'reset-password',
+    component:ResetPasswordComponent
+  },
+  {
+    path:'page-not-found',
+    component:PageNotFoundComponent
+  },
+  {
+    path:'**',
+    pathMatch:'full',
+    component:PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
